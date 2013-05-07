26f8d162d699b1e2663eb81a90b924f1cd05b647
Justin Zaun <justin.zaun@gmail.com>
2013-05-06 18:12:38 -1000

    Let text sections have links

--- classes/Plugins/Text.php
+++ classes/Plugins/Text.php
@@ -38,6 +38,35 @@ class Text {
     public function htmlValue($request, $id) {
         $raw = $this->rawValue($request, $id);
         $html = $this->rawToHtml($raw);
+
+        // external link
+        $html = preg_replace_callback(
+            '/(\[)(.*?)(\])(\(((http[s]?)|ftp):\/\/)(.*?)(\))/',
+            function ($match) {
+                $link = strtolower(str_replace(" ", "+", trim($match[7])));
+                $ret = "<a target='_blank' href='" . strtolower($match[5]) . "://" . $link . "'>" . trim($match[2]) . "</a>";
+                if (strtolower($match[5]) == "http") {
+                    $ret .= "<img src='/images/link_http.png' class='link' />";
+                } else if (strtolower($match[5]) == "https") {
+                    $ret .= "<img src='/images/link_https.png' class='link' />";
+                } else if (strtolower($match[5]) == "ftp") {
+                    $ret .= "<img src='/images/link_ftp.png' class='link' />";
+                }
+                return $ret;
+            },
+            $html
+        );
+
+        // internal link
+        $html = preg_replace_callback(
+            '/(\[)(.*?)(\])/',
+            function ($match) {
+                $link = str_replace(" ", "+", trim($match[2]));
+                return "<a href='" . $link . "'>" . trim($match[2]) . "</a>";
+            },
+            $html
+        );
+        
         return $html;
     }
     
