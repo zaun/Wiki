892f1b63447dc2f19f05c9d98ccd75e530a00035
Justin Zaun <justin.zaun@gmail.com>
2013-06-06 07:07:55 -1000

    Update Cast List html a bit.

--- classes/Plugins/Sections/CastList.php
+++ classes/Plugins/Sections/CastList.php
@@ -12,4 +12,32 @@ class CastList extends \App\Sections\BaseList {
     protected $placeholderTitle         = "Character Name";
     protected $placeholderDetail        = "Actor or Actress Name";
     protected $placeholderAdditional    = "Short description of role...";
+
+    protected $dataTitle      = "charachter";
+    protected $dataDetail     = "actor";
+    protected $dataAdditional = "description";
+
+    public function convertRawToHtml($raw) {
+        $items = json_decode($raw);
+        $html = "<ul>";
+        foreach ($items as $i) {
+            $order = $i->itemOrder;
+            $character = $i->{$this->dataTitle};
+            $actor = $i->{$this->dataDetail};
+            $description = $i->{$this->dataAdditional};
+            
+            if (!empty($character)) {
+                $html .= "<li><strong>" . $character . "</strong>";
+                if (!empty($actor)) {
+                    $html .= " portrayed by <em>" . $actor . "</em>";
+                }
+                if (!empty($description)) {
+                    $html .= "<div>" . $description . "</div>";
+                }
+                $html .= "</li>";
+            }
+        }
+        $html .= "</ul>";
+        return $html;
+    }
 }
