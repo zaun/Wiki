e8ed57d75dcd37d2b523b099831a43c8dc126816
Justin Zaun <justin.zaun@gmail.com>
2013-06-07 07:37:53 -1000

    Started to add example person article

---
+++ assets/migrations/3_example_person.php
@@ -0,0 +1,29 @@
+<?php
+return array(
+	'articles' => array (
+		'_data' => array(
+		    'up' => array(
+		        'insert' => array(
+                    array('id' => 3, 'template_id' => 2, 'title' => 'Buzz Aldrin', 'url' => 'Buzz_Aldrin', 'summary' => 'Harry Potter and the Philosopher\'s Stone is the first novel in the Harry Potter series written by J. K. Rowling and featuring Harry Potter, a young wizard. It describes how Harry discovers he is a wizard, makes close friends and a few enemies at the Hogwarts School of Witchcraft and Wizardry, and with the help of his friends thwarts an attempted comeback by the evil wizard Lord Voldemort, who killed Harry\'s parents when Harry was one year old.
+                    
+The book, which is J.K. Rowling\'s debut novel, was published on 26 June 1997 by Bloomsbury in London. In 1998 Scholastic Corporation published an edition for the United States market under the title Harry Potter and the Sorcerer\'s Stone. The novel won most of the British book awards that were judged by children, and other awards in the US. The book reached the top of the New York Times list of best-selling fiction in August 1999, and stayed near the top of that list for much of 1999 and 2000. It has been translated into several other languages and has been made into a feature-length film of the same name.', 'summary_html' => 'Buzz Aldrin (born Edwin Eugene Aldrin, Jr.; January 20, 1930) is an American <a href=\'/astronaut\'>astronaut</a>, and the <a href=\'/Apollo_11#Lunar_surface_operations\'>second person to walk on the Moon</a>. He was the <a href=\'/lunar_module\'>lunar module</a> pilot on <a href=\'/Apollo_11\'>Apollo 11</a>, the first manned lunar landing in history. On July 21, 1969, he set foot on the <a href=\'/Moon\'>Moon</a>, following mission commander <a href=\'/Neil_Armstrong\'>Neil Armstrong</a>. He is also a retired <a href=\'/United_States_Air_Force\'>United States Air Force</a> <a href=\'/aviator\'>pilot</a>.<br />'),
+                ),
+		    ),
+		),
+	),
+
+	'articleSections' => array (
+		'_data' => array(
+		    'up' => array(
+		    ),
+		),
+	),
+
+	'articleAttributes' => array (
+		'_data' => array(
+		    'up' => array(
+		    ),
+		),
+	),
+
+);
