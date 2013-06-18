ba6640d3893d8ead50be85aa0e612386120bc44c
Justin Zaun <justin.zaun@gmail.com>
2013-06-18 07:19:48 -1000

    More example updates

--- assets/migrations/3_example_person.php
+++ assets/migrations/3_example_person.php
@@ -4,7 +4,7 @@ return array(
 		'_data' => array(
 		    'up' => array(
 		        'insert' => array(
-                    array('id' => 3, 'template_id' => 2, 'title' => 'Buzz Aldrin', 'url' => 'Buzz_Aldrin', 'summary' => 'Buzz Aldrin (born Edwin Eugene Aldrin, Jr.; {{Birth Date}}) is an American [astronaut], and the second person to walk on the Moon. He was the [lunar module] pilot on [Apollo 11], the first manned lunar landing in history. On July 21, 1969, he set foot on the [Moon], following mission commander [Neil Armstrong]. He is also a retired [United States Air Force] [pilot](Aviator).', 'summary_html' => 'Buzz Aldrin (born Edwin Eugene Aldrin, Jr.; {{Birth Date}}) is an American <a href=\'/astronaut\'>astronaut</a>, and the second person to walk on the Moon. He was the <a href=\'/lunar_module\'>lunar module</a> pilot on <a href=\'/Apollo_11\'>Apollo 11</a>, the first manned lunar landing in history. On July 21, 1969, he set foot on the <a href=\'/Moon\'>Moon</a>, following mission commander <a href=\'/Neil_Armstrong\'>Neil Armstrong</a>. He is also a retired <a href=\'/United_States_Air_Force\'>United States Air Force</a> <a href=\'/Aviator\'>pilot</a>.<br />'),
+                    array('id' => 3, 'template_id' => 2, 'title' => 'Buzz Aldrin', 'url' => 'Buzz_Aldrin', 'summary' => 'Buzz Aldrin (born Edwin Eugene Aldrin, Jr. on {{Birth Date}}) is an American [astronaut], and the second person to walk on the Moon. He was the [lunar module] pilot on [Apollo 11], the first manned lunar landing in history. On July 21, 1969, he set foot on the [Moon], following mission commander [Neil Armstrong]. He is also a retired [United States Air Force] [pilot](Aviator).', 'summary_html' => 'Buzz Aldrin (born Edwin Eugene Aldrin, Jr. on January 20th, 1930) is an American <a href=\'/astronaut\'>astronaut</a>, and the second person to walk on the Moon. He was the <a href=\'/lunar_module\'>lunar module</a> pilot on <a href=\'/Apollo_11\'>Apollo 11</a>, the first manned lunar landing in history. On July 21, 1969, he set foot on the <a href=\'/Moon\'>Moon</a>, following mission commander <a href=\'/Neil_Armstrong\'>Neil Armstrong</a>. He is also a retired <a href=\'/United_States_Air_Force\'>United States Air Force</a> <a href=\'/Aviator\'>pilot</a>.<br />'),
                 ),
 		    ),
 		),
@@ -14,7 +14,8 @@ return array(
 		'_data' => array(
 		    'up' => array(
 		        'insert' => array(
-                    array('article_id' => 3, 'section_id' => 2, 'raw' => 'Aldrin was born in {{Birth Location}}, to Edwin Eugene Aldrin, Sr., a career military man, and his wife Marion (née Moon). He is of Scottish, Swedish, and German ancestry. After graduating from [Montclair High School] in 1946, Aldrin turned down a full scholarship offer from the [Massachusetts Institute of Technology], and went to the [United States Military Academy] at [West Point, New York]. The nickname "Buzz" originated in childhood: the younger of his two elder sisters mispronounced "brother" as "buzzer", and this was shortened to Buzz. Aldrin made it his legal first name in 1988.', 'html' => '<div class=\'para\'>Aldrin was born in <span class=\'attr\'><a href=\'/Glen_Ridge,_New_Jersey\'>Glen Ridge, New Jersey</a></span>, to Edwin Eugene Aldrin, Sr., a career military man, and his wife Marion (n&eacute;e Moon). He is of Scottish, Swedish, and German ancestry. After graduating from <a href=\'/Montclair_High_School\'>Montclair High School</a> in 1946, Aldrin turned down a full scholarship offer from the <a href=\'/Massachusetts_Institute_of_Technology\'>Massachusetts Institute of Technology</a>, and went to the <a href=\'/United_States_Military_Academy\'>United States Military Academy</a> at <a href=\'/West_Point,_New_York\'>West Point, New York</a>. The nickname &quot;Buzz&quot; originated in childhood: the younger of his two elder sisters mispronounced &quot;brother&quot; as &quot;buzzer&quot;, and this was shortened to Buzz. Aldrin made it his legal first name in 1988.</div>'),
+                    array('article_id' => 3, 'section_id' => 3, 'raw' => '>>aldrin:200>>
+Aldrin was born in [{{Birth Location}}] on {{Birth Date}}, to {{father}}, a career military man, and his wife {{mother}}. He is of Scottish, Swedish, and German ancestry. After graduating from [Montclair High School] in 1946, Aldrin turned down a full scholarship offer from the [Massachusetts Institute of Technology], and went to the [United States Military Academy] at [West Point, New York]. The nickname "Buzz" originated in childhood: the younger of his two elder sisters mispronounced "brother" as "buzzer", and this was shortened to Buzz. Aldrin made it his legal first name in 1988.', 'html' => '<div class=\'para\'><img src=\'buzz_aldrin/media/aldrin/200\' alt=\'\' style=\'float:right;width:200px;margin-left:5px;\'>Aldrin was born in <a href=\'/Birth_Location\'>Glen Ridge, New Jersey</a> on January 20th, 1930, to Edwin Eugene Aldrin, Sr., a career military man, and his wife Marion. He is of Scottish, Swedish, and German ancestry. After graduating from <a href=\'/Montclair_High_School\'>Montclair High School</a> in 1946, Aldrin turned down a full scholarship offer from the <a href=\'/Massachusetts_Institute_of_Technology\'>Massachusetts Institute of Technology</a>, and went to the <a href=\'/United_States_Military_Academy\'>United States Military Academy</a> at <a href=\'/West_Point,_New_York\'>West Point, New York</a>. The nickname &quot;Buzz&quot; originated in childhood: the younger of his two elder sisters mispronounced &quot;brother&quot; as &quot;buzzer&quot;, and this was shortened to Buzz. Aldrin made it his legal first name in 1988.</div>'),
                  )
 		    ),
 		),
@@ -24,10 +25,17 @@ return array(
 		'_data' => array(
 		    'up' => array(
 		        'insert' => array(
-                    array('article_id' => 3, 'attribute_id' => 2, 'raw' => '1930-01-20', 'html' => 'January 20th, 1930'),
-                    array('article_id' => 3, 'attribute_id' => 3, 'raw' => '[Glen Ridge, New Jersey]', 'html' => '<a href=\'/Glen_Ridge,_New_Jersey\'>Glen Ridge, New Jersey</a>'),
-                    array('article_id' => 3, 'attribute_id' => 6, 'raw' => 'Male', 'html' => 'Male'),
-                    array('article_id' => 3, 'attribute_id' => 9, 'raw' => 'American', 'html' => 'American'),
+                    array('article_id' => 3, 'attribute_id' => 2,  'raw' => '1930-01-20', 'html' => 'January 20th, 1930'),
+                    array('article_id' => 3, 'attribute_id' => 3,  'raw' => '[Glen Ridge, New Jersey]', 'html' => '<a href=\'/Glen_Ridge,_New_Jersey\'>Glen Ridge, New Jersey</a>'),
+                    array('article_id' => 3, 'attribute_id' => 6,  'raw' => 'Male', 'html' => 'Male'),
+                    array('article_id' => 3, 'attribute_id' => 7,  'raw' => '[NASA] Astronaut', 'html' => '<a href=\'/NASA\'>NASA</a> Astronaut'),
+                    array('article_id' => 3, 'attribute_id' => 8,  'raw' => 'Walking on the Moon', 'html' => 'Walking on the Moon'),
+                    array('article_id' => 3, 'attribute_id' => 9,  'raw' => 'American', 'html' => 'American'),
+                    array('article_id' => 3, 'attribute_id' => 12, 'raw' => 'Marion', 'html' => 'Marion'),
+                    array('article_id' => 3, 'attribute_id' => 13, 'raw' => 'Edwin Eugene Aldrin, Sr.', 'html' => 'Edwin Eugene Aldrin, Sr.'),
+                    array('article_id' => 3, 'attribute_id' => 15, 'raw' => 'Lois Driggs Cannon', 'html' => 'Lois Driggs Cannon'),
+                    array('article_id' => 3, 'attribute_id' => 17, 'raw' => '2012-12-28', 'html' => 'December 28th, 2012'),
+                    array('article_id' => 3, 'attribute_id' => 21, 'raw' => 'Beverly Zile', 'html' => 'Beverly Zile'),
                  )
 		    ),
 		),
