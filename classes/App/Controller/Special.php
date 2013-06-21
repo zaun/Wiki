bfe15ca2cabd2518480a5713a065dc5b1ee04336
Justin Zaun <justin.zaun@gmail.com>
2013-06-20 18:37:33 -1000

    Don't go to special pages on random

--- classes/App/Controller/Special.php
+++ classes/App/Controller/Special.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 class Special extends \PHPixie\Controller {    
     
     function action_random() {
-        $count = $this->pixie->orm->get('article')->count_all();
-        $article = $this->pixie->orm->get('article')->offset(rand(0, $count - 1))->limit(1)->find();
+        do {
+            $count = $this->pixie->orm->get('article')->count_all();
+            $article = $this->pixie->orm->get('article')->offset(rand(0, $count - 1))->limit(1)->find();
+        } while (substr($article->url, 0, 1) == '~');
         return $this->response->redirect('/' . $article->title);
     }
 }
